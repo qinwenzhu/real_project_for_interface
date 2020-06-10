f4da35bcb6fe373beed7c78045cf07c94f8dd784
zhuwenqin <zhuwenqin_vendor@sensetime.com>
2020-06-10 16:09:12 +0800

    u

--- utils/handle_excel.py
+++ utils/handle_excel.py
@@ -24,22 +24,22 @@ class HandleExcel:
             operation_sheet = load_workbook.active
         else:
             operation_sheet = load_workbook[sheet_name]
-        return operation_sheet
+        return load_workbook, operation_sheet
 
     def read_all_data_from_excel(self):
         """ 读取指定表单内的全部的测试数据 """
 
         # 打开需要读取测试用例的文件目录
-        operation_sheet = self.open_file()
+        operation = self.open_file()
 
         # 读取测试用例的表头
-        case_til = tuple(operation_sheet.iter_rows(min_row=1, max_row=1, min_col=1, max_col=operation_sheet.max_column, values_only=True))[0]
+        case_til = tuple(operation[-1].iter_rows(min_row=1, max_row=1, min_col=1, max_col=operation[-1].max_column, values_only=True))[0]
         # print(case_til)
         # 定义列表来接收每一行的一条测试用例并追加到列表内
         result_list = []
         # 循环读取所有测试用例数据<除表头外>
         # print(tuple(self.operation_sheet.iter_rows(min_row=2, values_only=True)))
-        for data in tuple(operation_sheet.iter_rows(min_row=2, values_only=True)):
+        for data in tuple(operation[-1].iter_rows(min_row=2, values_only=True)):
             # case_data = dict(zip(case_til, data))
             # result_list.append(case_data)
             result_list.append(dict(zip(case_til, data)))
@@ -49,23 +49,16 @@ class HandleExcel:
         """ 读取指定表单内的指定行的测试数据 """
         return self.read_all_data_from_excel()[row-1]
 
-    def write_data_to_excel(self, **kwargs):
+    def write_data_to_excel(self, row, actual, result):
 
         # 打开需要读取测试用例的文件目录
-        # operation_sheet = self.open_file()
+        operation = self.open_file()
 
-        if kwargs is not None:
-            for key, value in kwargs.items():
-                print(key, value)
-               # print(kwargs[key])
-               # print(value)
-            print(kwargs.items())
+        # 分别向测试文件对应表单的倒数第一列和倒数第二列写上对应的用例实际结果和断言结果
+        operation[-1].cell(row=row, column=operation[-1].max_column-1, value=actual)
+        operation[-1].cell(row=row, column=operation[-1].max_column, value=result)
 
-        # for key in kwargs.keys():
-        #     pass
-
-            # 数据写入
-            # operation_sheet.cell(row=row, column=operation_sheet.max_column-2, value=args)
+        operation[0].save()
 
 
 if __name__ == '__main__':
@@ -74,7 +67,7 @@ if __name__ == '__main__':
 
     case_file = r"{}/device_case.xlsx".format(SharePath.DATA_FOLDER)
     sheet_name = "device"
-    data = {"actual": 1, "result": 2}
-    HandleExcel(case_file).write_data_to_excel(**data)
+
+    HandleExcel(case_file).write_data_to_excel(2, "actual", "result")
     # print(HandleExcel(case_file).read_all_data_from_excel())
     pass
